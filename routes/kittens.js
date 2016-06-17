fadbd7fefc003cd2fff73a0ec4f49d2f0b76c620
Stephanie Ogburn <stephanie.ogburn@gmail.com>
2016-06-17 09:28:27 -0600

    finished get routes

--- routes/kittens.js
+++ routes/kittens.js
@@ -13,7 +13,7 @@ router.get('/', function(req, res){
     }
     client.query('SELECT * FROM kittens', function(err, data){
       if (err){
-        return (console.error('error retrieving from database', err));
+        return console.error('error retrieving from database', err);
       }
       console.log(data);
       res.render('kittens/index', {kittens: data.rows});
@@ -25,5 +25,43 @@ router.get('/new', function(req, res){
   res.render('kittens/new');
 });
 
+router.get('/:id', function(req, res){
+  pg.connect(db_url, function(err, client, done){
+    done();
+    if (err){
+      return console.error('error fetching', err);
+    }
+    client.query('SELECT * FROM kittens WHERE id = $1', [req.params.id], function(err, data){
+      if(err){
+        return console.error('error retrieving from database', err);
+      }
+      var kitten = data.rows[0];// selects the data you got which is the first item in the rows array
+      if (!kitten){
+        res.redirect('/kittens');
+      }
+      res.render('kittens/show', {kitten: kitten}) // go to the kittens show page and the data it has access to is kitten
+    })
+  })
+})
+
+router.get('/:id/edit', function(req, res){
+  // connects to pg database with the specified url
+  pg.connect(db_url, function (err, client, done){
+    done();
+    if (err){
+      return console.error('error fetching', err);
+    }
+    client.query('SELECT * FROM kittens WHERE id = $1', [req.params.id], function(err, data){
+      if (err){
+        console.error('error retrieving from database', err);
+      }
+      var kitten = data.rows[0];
+      if (!kitten){
+        res.redirect('/puppies');
+      }
+      res.render('kittens/edit', {kitten: kitten})
+    })
+  })
+})
 
 module.exports = router;
